9784b91449ed32b3c4b08e619124303c37b60b5c
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2023-04-16 14:46:55 +0000

    fix database issues

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2023 at 01:08 AM
+-- Generation Time: Apr 16, 2023 at 04:45 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -35,7 +35,8 @@ CREATE TABLE `product` (
   `quantity` int(11) NOT NULL,
   `alert_threshold` int(11) NOT NULL,
   `last_entry_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  `last_release_date` timestamp NOT NULL DEFAULT current_timestamp()
+  `last_release_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `stock_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -48,8 +49,17 @@ CREATE TABLE `stock` (
   `stock_id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `description` varchar(20) DEFAULT NULL,
-  `last_edit` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `last_edit` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `creation_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`stock_id`, `name`, `description`, `last_edit`, `creation_date`) VALUES
+(1, 'IT shop', 'it accessories', '2023-04-16 14:37:59', '2023-04-16 14:37:59'),
+(2, 'Fruits', 'fih l9oo9a wl br9o9a', '2023-04-16 14:37:59', '2023-04-16 14:37:59');
 
 -- --------------------------------------------------------
 
@@ -60,9 +70,18 @@ CREATE TABLE `stock` (
 CREATE TABLE `stockownership` (
   `username` varchar(20) NOT NULL,
   `stock_id` int(11) NOT NULL,
-  `creation_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `ownership_start_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `role` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stockownership`
+--
+
+INSERT INTO `stockownership` (`username`, `stock_id`, `ownership_start_date`, `role`) VALUES
+('ouassima12', 2, '2023-04-15 21:50:25', 'view'),
+('yahya.lz', 1, '2023-04-15 20:20:35', 'edit'),
+('yousra.eb', 2, '2023-04-14 22:10:30', 'edit');
 
 -- --------------------------------------------------------
 
@@ -83,8 +102,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `first_name`, `last_name`, `email`, `password`) VALUES
-('ouassima12', 'Ouassima', 'Aboukhair', 'ouassima@gmail.com', '123456'),
-('test', 'test', 'test lastname', 'test@gmail.com', '123'),
+('ouassima12', 'ouassima', 'ab', 'ouassima@gmail.com', 'ouassima1-'),
 ('yahya.lz', 'Yahya', 'Lazrek', 'duo.lz.yahya@gmail.com', '123456'),
 ('yousra.eb', 'Yousra', 'Elbarreq', 'yousra@gmail.com', '123456');
 
@@ -96,7 +114,8 @@ INSERT INTO `user` (`username`, `first_name`, `last_name`, `email`, `password`) 
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`);
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `stock_id` (`stock_id`);
 
 --
 -- Indexes for table `stock`
@@ -124,16 +143,10 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `product`
---
-ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `stock`
 --
 ALTER TABLE `stock`
-  MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -143,7 +156,7 @@ ALTER TABLE `stock`
 -- Constraints for table `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `stock` (`stock_id`);
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`stock_id`) REFERENCES `stock` (`stock_id`);
 
 --
 -- Constraints for table `stockownership`
