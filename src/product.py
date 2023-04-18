df4216470368e1c0c4d34a0855b04e352ea5da41
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2023-04-18 00:22:34 +0000

    update the whole project structure

--- src/product.py
+++ src/product.py
@@ -3,13 +3,7 @@ from connect import *
 class Product:
     product_id = -1
     def __init__(self, name, description, unit_price, quantity, alert_threshold, last_entry_date, last_release_date):
-        self.name = name
-        self.description = description
-        self.unit_price = unit_price
-        self.quantity = quantity
-        self.alert_threshold = alert_threshold
-        self.last_entry_date = last_entry_date
-        self.last_release_date = last_release_date
+        set_product(self, name, description, unit_price, quantity, alert_threshold, last_entry_date, last_release_date)
     
     def __str__(self):
         product = f"Product ({self.product_id})\n"
@@ -25,19 +19,46 @@ class Product:
     def set_product_id(self, id):
         self.product_id = id
 
+    def set_product(self, name, description, unit_price, quantity, alert_threshold, last_entry_date, last_release_date):
+        self.name = name
+        self.description = description
+        self.unit_price = unit_price
+        self.quantity = quantity
+        self.alert_threshold = alert_threshold
+        self.last_entry_date = last_entry_date
+        self.last_release_date = last_release_date
+
     def add_product(self):
         conn = mysqlconnect()
         cur = conn.cursor()
 
-        cur.execute('INSERT INTO product(name, description, unit_price, quantity, alert_threshold, last_entry_date, last_release_date, stock_id) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)', self.name, self.description, self.unit_price, self.quantity, self.alert_threshold, self.last_entry_date, self.last_release_date, stock_id)
+        cur.execute('''INSERT INTO product(name, description, unit_price, quantity, alert_threshold, last_entry_date, last_release_date, stock_id) 
+                       VALUES(%s, %s, %s, %s, %s, %s, %s, %s)''', 
+                    (self.name, self.description, self.unit_price, self.quantity, self.alert_threshold, self.last_entry_date, self.last_release_date, stock_id))
         conn.commit()
         print('new product added')
         
         conn.close()
 
     def delete_product(self):
-        print("delete: " + str(self.product_id))
+        conn = mysqlconnect()
+        cur = conn.cursor()
+
+        cur.execute('DELETE FROM product WHERE product_id = %s', self.product_id)
+        conn.commit()
+        print('product deleted')
+        
+        conn.close()
     
     def update_product(self):
-        print("update: " + str(self.product_id))
+        conn = mysqlconnect()
+        cur = conn.cursor()
+
+        cur.execute('''UPDATE product 
+                       SET name = %s, description = %s, unit_price = %s, quantity = %s, alert_threshold = %s, last_entry_date = %s, last_release_date = %s, stock_id = %s 
+                       WHERE product_id = %s
+                    ''', (self.name, self.description, self.unit_price, self.quantity, self.alert_threshold, self.last_entry_date, self.last_release_date, stock_id, self.product_id))
+        conn.commit()
+        print('product updated')
         
+        conn.close()
