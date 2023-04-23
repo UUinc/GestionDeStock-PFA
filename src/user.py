353734ff5746a816a08fa3ff0792aa90c810fa8b
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2023-04-23 20:03:16 +0100

    fix issues in login and signup methods

--- src/user.py
+++ src/user.py
@@ -2,7 +2,6 @@ from connect import *
 from utils import *
 
 class User:
- 
     def __init__(self, username, first_name, last_name, email, password):
         self.__username = username
         self.__first_name = first_name
@@ -19,14 +18,23 @@ class User:
         conn.close()
         return output[0]==0
         
-    def signup(self, username, first_name,last_name,email,password):
+    def signup(self, username, first_name, last_name, email, password):
         conn = mysqlconnect()
         cur = conn.cursor()
-        cur.execute('''INSERT INTO User(username, first_name, last_name, email, password)
-                       VALUES(%s, %s, %s, %s, %s)''', 
-                    (self.__username, self.__first_name, self.__last_name, self.__email, self.__password))
-        conn.commit()
-        print('Signup successful')
+        try:
+            cur.execute('INSERT INTO user VALUES(%s, %s, %s, %s, %s)', (self.__username, self.__first_name, self.__last_name, self.__email, self.__password))
+            conn.commit()
+            print('new user added')
+            return True
+        except Exception as e:
+            match e.args[1].split()[5].strip('\''):
+                case 'PRIMARY':
+                    print("Username already exist")
+                case 'UC_email':
+                    print("Email already exist")
+                case _:
+                    print("error occured!")
+            return False
         conn.close()
 
     def get_information(self):
@@ -38,4 +46,4 @@ class User:
 #u = user('yousra1','yousra','elbq','yous@gmail.com','456')
 #u.login()
 User.login('ouass1','123')
-User.signup('yaya', 'yahya', 'lzrek', 'yaya@gmail.com', 'ya12')
+User.signup('yaya', 'yahya', 'lazrek', 'yaya@gmail.com', 'ya12')
